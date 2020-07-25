e98a3e25fe9e50157a8d25bdd9f6a5cbc4cac9d3
2breadkey <2breadkey@gmail.com>
2020-07-25 20:02:13 +0900

    implement and test xor

--- lib/models/perceptron.dart
+++ lib/models/perceptron.dart
@@ -6,6 +6,14 @@ num and(num x1, num x2) {
   return _calculateResult(x, w, b);
 }
 
+num or(num x1, num x2) {
+  final x = [x1, x2];
+  final w = [0.5, 0.5];
+  final b = -0.3;
+
+  return _calculateResult(x, w, b);
+}
+
 num nand(num x1, num x2) {
   final x = [x1, x2];
   final w = [-0.5, -0.5];
@@ -13,6 +21,8 @@ num nand(num x1, num x2) {
 
   return _calculateResult(x, w, b);
 }
+
+num xor(num x1, x2) => and(nand(x1, x2), or(x1, x2));
 
 num _calculateResult(List<num> x, List<num> w, num b) =>
     (x * w).sum + b <= 0 ? 0 : 1;
--- test/perceptron_test.dart
+++ test/perceptron_test.dart
@@ -9,10 +9,24 @@ void main() {
     expect(and(1, 1), 1);
   });
 
+  test("or test", () {
+    expect(or(0, 0), 0);
+    expect(or(0, 1), 1);
+    expect(or(1, 0), 1);
+    expect(or(1, 1), 1);
+  });
+
   test("nand test", () {
     expect(nand(0, 0), 1);
     expect(nand(0, 1), 1);
     expect(nand(1, 0), 1);
     expect(nand(1, 1), 0);
+  });
+
+  test("xor test", () {
+    expect(xor(0, 0), 0);
+    expect(xor(0, 1), 1);
+    expect(xor(1, 0), 1);
+    expect(xor(1, 1), 0);
   });
 }
